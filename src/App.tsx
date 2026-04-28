2cb15f877d40dbf4ece8d37f3520e601f213bf93
Colin Groark <colin@nexleaf.org>
2026-04-28 11:47:03 +0100

    basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,12 @@ import './App.css'
 function App() {
   const [quotes, setQuotes] = useState<Quote[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
-  const [total, setTotal] = useState<number>(0);
   const [error, setError] = useState<string>('');
+  const [total, setTotal] = useState<number>(0);
 
   useEffect(() => {
     setLoading(true);
     setError('');
-
     const fetchQuotes = async () => {
       try {
         const res = await fetch(`https://dummyjson.com/quotes`);
@@ -20,35 +19,46 @@ function App() {
         const data: QuoteJSONResponse = await res.json();
         setQuotes(data.quotes);
         setTotal(data.total);
-        console.log('quotes', quotes)
       }
       catch(err) {
-        setError(err instanceof Error ? err.message : String(err))
+        setError(err instanceof Error ? err.message : String(err));
       }
       finally {
         setLoading(false);
       }
     }
     fetchQuotes();
-
   }, [])
 
   return (
     <>
-    <div>Content</div>
-    {loading && <div>Loading!</div>}
-      {quotes.length > 0 && !loading && !error &&
-        <ul>
-          {quotes.map((each) =>
-            <li key={each.id}>
-              {each.quote}
-            </li>
-          )}
-        </ul>
+      <h1>Quote Check</h1>
+      {quotes.length > 0 && <h2>{total} Found!</h2>}
+      {loading && !error && <p>Loading...</p>}
+      {!loading && error && <p>Error: {error}</p>}
+      {!loading && !error && quotes.length > 0
+        ? (
+           <div>
+            <ul className="quote-section">
+              {quotes.map((each) =>
+                <li key={each.id}>
+                  <div className="quote-block">
+                    <h3>{each.author}</h3>
+                    <p>{each.quote}</p>
+                  </div>
+                </li>
+              )}
+            </ul>
+          </div>
+        )
+        : (
+          <p>No Quotes found!</p>
+        )
       }
-    </>
 
+    </>
   )
+
 }
 
 export default App;
