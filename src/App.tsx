af0335eeb2f4f584cf5f0a6cca6a5c97ce141ffd
Colin Groark <colin@nexleaf.org>
2026-05-01 20:48:51 +0100

    filter local

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, use } from 'react';
 import type { Quote, QuoteJSONResponse } from './interfaces/quotes';
 import './App.css'
 
@@ -8,17 +8,25 @@ function App() {
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
   const [total, setTotal] = useState<number>(0);
+  const [query, setQuery] = useState<string>('');
+  const [debounceQuery, setDebounceQuery] = useState<string>('');
+
+  useEffect(() => {
+    const debounceHandler = setTimeout(() => setDebounceQuery(query), 300);
+    return () => clearTimeout(debounceHandler);
+  }, [query])
 
   useEffect(() => {
     setLoading(true);
     setError('');
+
     const fetchQuotes = async () => {
       try {
         const res = await fetch(`https://dummyjson.com/quotes`);
-        if(!res.ok) throw new Error('err in fetch');
+        if(!res.ok) throw new Error('failed to fetch');
         const data: QuoteJSONResponse = await res.json();
         setQuotes(data.quotes);
-        setTotal(data.total);
+        setTotal(data.total)
       }
       catch(err) {
         setError(err instanceof Error ? err.message : String(err));
@@ -27,25 +35,27 @@ function App() {
         setLoading(false);
       }
     }
+
     fetchQuotes();
-  }, [])
+  }, []);
+
+  const filteredQuotes = quotes.filter((each) => each.quote.toLowerCase().includes(debounceQuery.toLowerCase().trim()));
 
   return (
     <>
       <h1>Quote Check</h1>
-      {quotes.length > 0 && <h2>{total} Found!</h2>}
+      {filteredQuotes.length > 0 && <h2>{total} Found!</h2>}
+      <label htmlFor="search"></label>
+      <input type="text" name="search" id="search" value={query} onChange={(e) => setQuery(e.target.value)} />
       {loading && !error && <p>Loading...</p>}
       {!loading && error && <p>Error: {error}</p>}
       {!loading && !error && quotes.length > 0
         ? (
            <div>
             <ul className="quote-section">
-              {quotes.map((each) =>
+              {filteredQuotes.map((each) =>
                 <li key={each.id}>
-                  <div className="quote-block">
-                    <h3>{each.author}</h3>
-                    <p>{each.quote}</p>
-                  </div>
+                  {each.quote}
                 </li>
               )}
             </ul>
