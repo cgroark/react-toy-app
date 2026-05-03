9993a9f5729b351b52079f81f065092b555176a9
Colin Groark <colin@nexleaf.org>
2026-05-03 13:26:29 +0100

    paginate

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, use } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import type { Product, JSONResponse } from './interfaces/quotes';
 import './App.css'
 
@@ -13,19 +13,27 @@ function App() {
   const [limit, setLimit] = useState<number>(10);
   const [skip, setSkip] = useState<number>(0);
 
+  const params = useMemo(() => {
+    const p = new URLSearchParams({
+      ...(debounceSearch && {q: debounceSearch}),
+      ...({limit: String(limit)}),
+      ...({skip: String(skip)}),
+    })
+    return p;
+  }, [debounceSearch, limit, skip]);
+
   useEffect(() => {
     const debounceHandler = setTimeout(() => setDebounceSearch(search), 300);
     return () => clearTimeout(debounceHandler);
   }, [search])
 
   useEffect(() => {
-    if(!debounceSearch) return;
     const fetchProducts = async () => {
       setLoading(true);
       setError('');
 
       try {
-        const res = await fetch(`https://dummyjson.com/products/search?q=${debounceSearch}&limit=${limit}&skip=${skip}`);
+        const res = await fetch(`https://dummyjson.com/products/search?${params}`);
         if(!res.ok) throw new Error('failed to fetch');
         const data: JSONResponse = await res.json();
         setProducts(data.products);
@@ -39,18 +47,23 @@ function App() {
       }
     }
     fetchProducts();
-  }, [debounceSearch, limit, skip])
+  }, [params])
 
   return (
     <>
       <h1>Product Check</h1>
       <label style={{paddingRight: '10px'}} htmlFor="search">Find Products</label>
-      <input type="text" id="search" name="search" placeholder="search..." value={search} onChange={(e) => setSearch(e.target.value)} />
+      <input type="text" id="search" name="search" placeholder="search..." value={search} onChange={(e) => {
+        setSearch(e.target.value)
+        setLimit(10);
+        setSkip(0);
+        }
+      } />
       {products.length > 0 && <h2>{total} Found!</h2>}
       {loading && !error && <p>Loading...</p>}
       {!loading && error && <p>Error: {error}</p>}
       {!loading && !error && products.length > 0
-        ? (
+        && (
            <div>
             <ul className="quote-section">
               {products.map((each) =>
@@ -62,18 +75,28 @@ function App() {
             </ul>
           </div>
         )
-        : (
-          <p>No Quotes found!</p>
-        )
       }
+      {!loading && !error && debounceSearch && products.length === 0 &&  (
+        <p>No Quotes found!</p>
+      )}
       <label htmlFor="items" style={{paddingRight: '10px'}}>Items Per page</label>
-      <select name="items" id="items" value={limit} onChange={(e) => setLimit(Number(e.target.value))}>
-        <option value="limit">Set Limit</option>
+      <select name="items" id="items" value={limit} onChange={(e) => {
+        setLimit(Number(e.target.value))
+        setSkip(0);
+        }
+      }>
+        <option value="limit" disabled>Set Limit</option>
         {[5,10,25].map((each) =>
           <option key={each} value={each}>{each}</option>
         )}
       </select>
-
+      <div className="page-buttons">
+        <button onClick={() => setSkip((prev) => prev - limit)} disabled={(skip-limit < 0)}>Prev</button>
+        <button onClick={() => setSkip((prev) => prev+limit)} disabled={total < skip + limit}>Next</button>
+      </div>
+      <div>
+        Page {Math.floor(skip/limit) + 1} of {Math.ceil(total / limit)}
+      </div>
     </>
   )
 
